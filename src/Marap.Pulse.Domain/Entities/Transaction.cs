58f1cb59bc0463445346b23367920cdf8f15b960
Ales Klenka <ales.klenka@marap.com>
2025-09-24 13:21:49 +0200

    Adding LocationId to Transaction

--- src/Marap.Pulse.Domain/Entities/Transaction.cs
+++ src/Marap.Pulse.Domain/Entities/Transaction.cs
@@ -7,14 +7,16 @@ public class Transaction : Entity<int>
 {
     public int PartId { get; private set; }
     public ChangeAmount ChangeAmount { get; private set; }
+    public int LocationId { get; private set; }
     public string Type { get; private set; } // e.g. Receipt, Consume, Adjust
     public DateTime Timestamp { get; private set; }
 
-    public Transaction(int id, int partId, ChangeAmount changeAmount, string type, DateTime timestamp)
+    public Transaction(int id, int partId, ChangeAmount changeAmount, int locationId, string type, DateTime timestamp)
         : base(id)
     {
         PartId = partId;
         ChangeAmount = changeAmount;
+        LocationId = locationId;
         Type = type;
         Timestamp = timestamp;
     }
